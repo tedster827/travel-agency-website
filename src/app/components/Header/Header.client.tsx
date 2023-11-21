c7d4abef8c3868dd061d1f11326d6a01d5f9af06
Teddy Williams <TedsterDev@gmail.com>
2023-11-20 16:11:03 -0800

    feat(Header.client.tsx): adds Navigation bar as a new sub component

--- src/app/components/Header/Header.client.tsx
+++ src/app/components/Header/Header.client.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from "react";
 import NavigationBar from "./NavigationBar.client"
+import Link from "next/link";
 import { useRouter } from "next/navigation"
 // NOTE: Styles still need to be imported
 
@@ -12,13 +13,12 @@ import { useRouter } from "next/navigation"
  *
  */
 const Header: React.FunctionComponent = () => {
+    const router = useRouter()
     return (
         // Main Header Container
         <header id={"header"} className={"bg-white text-gray-800 shadow-md"}>
             <h1 id={"logo"} className={"text-2xl font-bold italic mx-4 my-2"}>
-                <a href="page.tsx" className={"text-blue-600 hover:text-blue-800"}>Blissful Cruises
-                    <span>Cruise and Travel Specialists</span>
-                </a>
+                <Link href={"/"} className={"text-blue-600 hover:text-blue-800"} >Blissful Cruises | Cruise and Travel Specialists</Link>
             </h1>
             <NavigationBar/>
         </header>
