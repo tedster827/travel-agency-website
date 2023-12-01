91da6596c528a3fc40d93715022b88072bf0fa92
Teddy Williams <TedsterDev@gmail.com>
2023-12-01 14:57:02 -0800

    feat(Header Components): adds version 1 of the headers with notes for known issues

--- src/app/components/Header/NavigationBar.client.tsx
+++ src/app/components/Header/NavigationBar.client.tsx
@@ -2,89 +2,76 @@
 
 import React, {useRef, useState} from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
-// Font Awesome (Icon Library and Toolkit) fontawesome.com
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import NavigationBarMenu from "src/app/components/Header/NavigationBarMenu.client";
 
+interface MenuItem {
+    name: string
+    linkPath?: string
+}
 
 /**
  * NavigationBar (FC Functional Component)
  *
  * This component renders the top navigation bar of the website,
  * including the logo and navigation links.
+ *
+ * FIXME: In narrower resolutions the elements need to change and some need to be hidden, like maybe the slogan.
+ *
+ * FIXME: On normal desktop resolutions the elements are too close together
+ *
  */
 
 const NavigationBar: React.FunctionComponent = () => {
-    // State hook for managing the dropdown open/close
-    const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
-    // Timeout for dropdown visibility
-    const dropdownTimeoutId = useRef<NodeJS.Timeout | null>(null)
-    // Ref for the menu container to track its DOM element (Also tells TypeScript the type of object menuRef.current
-    const menuRef = useRef<HTMLLIElement>(null);
 
-    // Hook for accessing the Next.js router instance
-    const router = useRouter()
+    const listOfMenuItems: MenuItem[] = [
+        { name: "Home", linkPath: "/" },
+        { name: "Users", linkPath: "/pages/users" }
+    ]
 
-    // Event handler for clicking the dropdown toggle button
-    const toggleDropdown = (isVisible: boolean) => {
-        if (dropdownTimeoutId.current != null) {
-            clearTimeout(dropdownTimeoutId.current)
-        }
 
-        if(isVisible) {
-            setIsDropdownVisible(true);
-        } else {
-            // Set delay before hiding the dropdown
-            dropdownTimeoutId.current = setTimeout(() => {
-                setIsDropdownVisible(false);
-            }, 300) // Timeout for 3 seconds (300 milliseconds)
-        }
-    }
 
     return (
-        <nav id={"nav"} className={"bg-gray-800 p-4"}>
-            {/*Link component for client-side routing to the home page*/}
-            <ul className={"flex items-center justify-between"}>
-                <li>
-                    <Link href={"/"} className={"text-blue-50 hover:text-blue-800"} >Blissful Cruises | Cruise and Travel Specialists</Link>
-                    <br></br>
-                    <Link className={"text-white"} href={"/"}>Welcome Aboard!</Link>
-                </li>
-                {/*Dropdown with click and hover functionalities*/}
-                <li
-                    // Positioning for dropdown
-                    className={"relative"}
-                    // Attach ref to the list item
-                    ref={menuRef}
-                    onMouseEnter={() => toggleDropdown(true) }
-                    onMouseLeave={() => toggleDropdown(false) }
+        <nav
+            id="nav"
+            className={
+            "navbar " +
+                "bg-base-100 " +
+                "space-x-10 "
+            }
+            aria-label={"Navigation Bar"}
+        >
+            <div
+                className={"flex-1000"}
+            >
+                <Link
+                    href={"/"}
+                    className={"btn btn-ghost text-xl"}
                 >
-                    {/*Button to toggle the dropdown menu*/}
-                    <button
-                        className={"flex items-center text-white"}
-                        onClick={() => {
-                            setIsDropdownVisible(!isDropdownVisible);
-                        }}
-                    >
-                        <FontAwesomeIcon icon={faChevronDown} className={"mr-2"}></FontAwesomeIcon>
-                        Site Map
-                    </button>
-                    {isDropdownVisible && (
-                        <ul
-                            className={"absolute bg-gray-700 p-2 mt-2 rounded shadow-lg"}
-                            onMouseEnter={() => toggleDropdown(true) }
-                            onMouseLeave={() => toggleDropdown(false) }
-                        >
-                            <li>
-                                <Link href={"/"}>Home</Link>
-                            </li>
-                            {/*{TODO: Add other navigation items as they are made}*/}
-                        </ul>
-                    )}
-                </li>
-            </ul>
+                    Blissful Cruises
+                </Link>
+                <h2
+                    // className={""}
+                >
+                    The Cruise and Travel Specialists
+                </h2>
+            </div>
+            <div
+                className={"flex-none"}
+            >
+                {/*Link component for client-side routing to the home page*/}
+                <ul className={"menu menu-horizontal px-1"}>
+                    <li>
+                        <Link href={'/'}>Home</Link>
+                    </li>
+                    <li>
+                        <NavigationBarMenu
+                                    menuName={"Site Map"}
+                                    listOfMenuItems={listOfMenuItems}
+                                />
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
