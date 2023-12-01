c07dbebd91abb544c77bfd43302854336316cadf
Teddy Williams <TedsterDev@gmail.com>
2023-12-01 14:45:48 -0800

    feat(pages/users): adds a users page for later admin use

---
+++ src/app/pages/users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewUserPage: React.FunctionComponent = () => {
+    return(
+        <>
+            <h1>NewUserPage</h1>
+        </>
+    )
+}
+
+export default NewUserPage;
