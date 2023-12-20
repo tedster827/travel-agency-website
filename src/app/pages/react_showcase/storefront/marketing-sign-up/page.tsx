656a191a4ada8f122177f2d02d1fa9a8c33eb721
Teddy Williams <TedsterDev@gmail.com>
2023-12-19 18:22:35 -0800

    feat(Mock storefront: sign up): adds new form component with form validation

--- src/app/pages/react_showcase/storefront/marketing-sign-up/page.tsx
+++ src/app/pages/react_showcase/storefront/marketing-sign-up/page.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 // NOTE: import Form from "src/app/components/Generic/Form";
 import FormShorterWithoutUsingRef from "src/app/components/Generic/FormShorterWithoutUsingRef";
+import FormUsingReactHookForm from "src/app/components/Generic/FormUsingReactHookForm"
 
 const MarketingSignUpPage: React.FunctionComponent = () => {
     return (
         <div>
-            <FormShorterWithoutUsingRef name={"Email Sign Up"}/>
+            {/*<FormShorterWithoutUsingRef name={"Email Sign Up"}/>*/}
+            <FormUsingReactHookForm formName={"Email Sign Up"}/>
         </div>
     )
 }
